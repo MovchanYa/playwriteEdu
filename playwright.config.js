95476d9e415adadab22f6ecbb45d931a3da475c7
Yaroslav <0chanbanan0@gmail.com>
2026-05-31 14:05:59 +0300

    гpdate playwright.config.js
    third attempt

--- playwright.config.js
+++ playwright.config.js
@@ -18,6 +18,7 @@ export default defineConfig({
   // globalSetup: require.resolve("./global-setup"),
   // globalTeardown: require.resolve("./global-teardown"),
   timeout: 10000,
+  testIgnore: '**/lesson23.spec.js',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
